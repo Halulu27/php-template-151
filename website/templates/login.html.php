b3f88302741b6580af3e03b5fb72a369b4bfd250
halulu27 <simon.odermatt@hotmail.ch>
2017-03-16 15:59:51 +0100

    Implement login

--- website/templates/login.html.php
+++ website/templates/login.html.php
@@ -1,1 +1,8 @@
-GUGUS
+
+ <form method="POST">
+  Email:<br>
+  <input type="text" name="email" value=""><br>
+  Password:<br>
+  <input type="text" name="password" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
--- website/web/index.php
+++ website/web/index.php
@@ -4,13 +4,27 @@ error_reporting(E_ALL);
 
 require_once("../vendor/autoload.php");
 $tmpl = new halulu27\SimpleTemplateEngine(__DIR__ . "/../templates/");
+$pdo = new \PDO(
+		"mysql:host=mariadb;dbname=app;charset=utf8",
+		"root",
+		"my-secret-pw",
+		[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+		);
 
 switch($_SERVER["REQUEST_URI"]) {
 	case "/":
 		(new halulu27\Controller\IndexController($tmpl))->homepage();
 		break;
 	case "/login":
-		(new halulu27\Controller\LoginController($tmpl))->showLogin();
+		$cnt = (new halulu27\Controller\LoginController($tmpl, $pdo));
+		if ($_SERVER["REQUEST_METHOD"] === "GET")
+		{
+			$cnt->showLogin();
+		}
+		else 
+		{
+			$cnt->login($_POST);
+		}
 		break;
 	//case "/test/upload":
 		//if(file_put_contents(__DIR__ . "/../../upload/test.txt", "Mein erster Upload")) {
